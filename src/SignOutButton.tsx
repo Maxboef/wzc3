6ffbdc29e309810649ef6debee30da5f58996475
Max <max@ilmer.nl>
2023-11-19 23:50:07 +0100

    Compress images

--- src/SignOutButton.tsx
+++ src/SignOutButton.tsx
@@ -8,10 +8,10 @@ function SignOutButton() {
 
   return (
     <div
-      className="inline px-4 py-1 bg-blue-950 text-white rounded"
+      className="px-4 py-1 bg-blue-950 text-white rounded border border-slate-600 flex items-center cursor-pointer"
       onClick={logOut}
     >
-      Log out
+      Log uit
     </div>
   );
 }
