26fb5a4139945a0dad560cb08194829e0b00a562
Max <max@ilmer.nl>
2023-12-16 00:09:44 +0100

    First version played matches

---
+++ src/pages/PouleResults.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { Club } from "../types/Club";
+import Logo from "../assets/logo.png";
+import MatchView from "../components/molecules/MatchView";
+
+function PouleResults() {
+  const [results, setResults] = useState<Club[]>([]);
+
+  function formatPath(relationCode: string) {
+    if (relationCode === "BBKZ50J") {
+      return Logo;
+    }
+
+    return "https://logoapi.voetbal.nl/logo.php?clubcode=" + relationCode;
+  }
+
+  async function getPouleResults() {
+    const response = await fetch(
+      "https://data.sportlink.com/pouleuitslagen?aantaldagen=90&weekoffset=-10&poulecode=720229&eigenwedstrijden=NEE&gebruiklokaleteamgegevens=NEE&client_id=bybEeY5S2Y"
+    );
+    const data = await response.json();
+
+    setResults(data);
+  }
+
+  useEffect(() => {
+    getPouleResults();
+  }, []);
+
+  return <></>;
+}
+
+export default PouleResults;
